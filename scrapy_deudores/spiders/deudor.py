079310a5bad8c5186a6802b2d491d5f9490462fa
aniversarioperu <aniversarioperu1@gmail.com>
2014-08-02 01:57:38 +0300

    avoid line break in between values

--- scrapy_deudores/spiders/deudor.py
+++ scrapy_deudores/spiders/deudor.py
@@ -10,7 +10,7 @@ locale.setlocale(locale.LC_ALL, "en_US.UTF8")
 class DeudorSpider(scrapy.Spider):
     name = "deudor"
     allowed_domains = ["pisaq.minjus.gob.pe"]
-    max_id = 1000
+    max_id = 2000
 
     def start_requests(self):
         for i in range(self.max_id):
@@ -23,20 +23,20 @@ class DeudorSpider(scrapy.Spider):
         sel = response.xpath
 
         sele = sel("//td[@class='formularioColumna']/text()").extract()
-        item['nombres'] = sele[2].strip()
-        item['apellidos'] = sele[5].strip()
-        item['dni'] = sele[8].strip()
-        item['domicilio'] = sele[11].strip()
-        item['delito'] = sele[14].strip()
+        item['nombres'] = sele[2].strip().replace("\n", " -")
+        item['apellidos'] = sele[5].strip().replace("\n", " -")
+        item['dni'] = sele[8].strip().replace("\n", " -")
+        item['domicilio'] = sele[11].strip().replace("\n", " -")
+        item['delito'] = sele[14].strip().replace("\n", " -")
 
         # check!
-        item['entidad_agraviada'] = sele[17].strip()
+        item['entidad_agraviada'] = sele[17].strip().replace("\n", " -")
 
-        item['fecha_sentencia'] = sele[20].strip()
-        item['fecha_ejecutoria'] = sele[23].strip()
-        item['juzgado'] = sele[26].strip()
-        item['expediente'] = sele[29].strip()
-        item['solidaria'] = sele[32].strip()
+        item['fecha_sentencia'] = sele[20].strip().replace("\n", " -")
+        item['fecha_ejecutoria'] = sele[23].strip().replace("\n", " -")
+        item['juzgado'] = sele[26].strip().replace("\n", " -")
+        item['expediente'] = sele[29].strip().replace("\n", " -")
+        item['solidaria'] = sele[32].strip().replace("\n", " -")
 
         sele = sel("//td[@class='formularioColumna']/div[@align='center']/text()").extract()
         item['reparacion_civil'] = locale.atof(sele[0])
@@ -44,6 +44,6 @@ class DeudorSpider(scrapy.Spider):
         item['monto_total'] = locale.atof(sele[2])
         item['pagos_realizados'] = locale.atof(sele[3])
         item['pagos_pendientes'] = locale.atof(sele[4])
-        item['url'] = response.url
+        item['url'] = response.url.replace("\n", " -")
 
         yield item
